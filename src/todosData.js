a057ef9a040713b620f5e8ce8445e526504bcadb
Aleksandre Lashauri <aleksandrelashauri@gmail.com>
2021-06-09 16:43:53 +0400

    checkbox

---
+++ src/todosData.js
@@ -0,0 +1,15 @@
+const todosData = [
+    {
+        id: 1,
+        text: "noMoney",
+        completed: false
+    },
+    {
+        id:2,
+        text:"noHoney",
+        completed:true
+    }
+  
+]
+
+export default todosData
