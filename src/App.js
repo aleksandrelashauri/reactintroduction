ca891afa14c5fc50ef6f6174d03114e25a5b7a72
Aleksandre Lashauri <aleksandrelashauri@gmail.com>
2021-06-13 23:00:48 +0400

    ongoing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
- import React from "react"
- import TodoItem from "./TodoItem"
- import todosData from "./todosData"
- 
- class App extends React.Component {
-     constructor() {
-         super()
-         this.state = {
-             todos: todosData
-         }
-         this.handleChange = this.handleChange.bind(this)
-     }
-     
-     handleChange(id) {
-         this.setState(prevState => {
-             const updatedTodos = prevState.todos.map(todo => {
-                 if (todo.id === id) {
-                     todo.completed = !todo.completed
-                 }
-                 return todo
-             })
-             return {
-                 todos: updatedTodos
-             }
-         })
-     }
-     
-     render() {
-         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-         
-         return (
-             <div className="todo-list">
-                 {todoItems}
-             </div>
-         )    
-     }
- }
- 
- export default App
+  
+import React from 'react'
+
+import Main from "./components/Main/Main";
+import Detailed from "./components/Detailed/Detailed";
+import Days from "./components/Days/Days";
+
+
+function App() {
+
+    return(
+    <div className='main-container'>
+        <Main />
+       <Detailed />
+           {/*<Days />*/}  
+
+    </div>
+    );
+}
+
+export default App;
+
---
+++ src/components/Detailed/Detailed.js
@@ -0,0 +1,81 @@
+import React,{useState} from 'react';
+
+const api={
+    key: 'be20f40819aa6518ef1d8eeffc368109',
+    base: 'https://api.openweathermap.org/data/2.5/'
+}
+function Detailed() {
+const [query, setQuery]= useState('');
+const [weather, setWeather]= useState({});
+
+const search = evt => {
+    if(evt.key === 'Enter'){
+        fetch(`${api.base}forecast/daily?q=${query}&units=metric&cnt=8&APPID=${api.key}`)
+        .then(res => res.json() )
+        .then(result => {
+            setWeather(result);
+            setQuery('');
+            console.log(result);
+        });
+    }
+}
+
+ const dateBuilder = (d) => {
+     let months=["January","February","March","April","May","June","July",
+     "August","September","October","November","December"];
+     let days = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+     let day = days[d.getDay()];
+     let date = d.getDate();
+     let month = months[d.getMonth()];
+     let year = d.getFullYear();
+
+     return `${day} ${date} ${month} ${year}`
+ }  
+ 
+    return (
+        <div className="app">
+         <main>
+            <div className="search-box">
+                 <input type="text"
+                  className="search-bar" 
+                  placeholder="search..." 
+                  onChange= { e  => setQuery(e.target.value)}
+                  value={query}
+                  onKeyPress={search}
+                     />
+                 </div>
+        
+             {(typeof weather.main !== "undefined" ) ? (
+                <div>
+                    <div className="date">
+                     {dateBuilder(new Date())}  
+                      </div>
+                 <div className="location-box">
+                   <div className="location">
+                    {weather.name},
+                    {weather.sys.country}   
+                    </div>
+                    
+                </div>
+                     <div className="weather-box">
+                     <div className="temp">
+                      {Math.round(weather.main.temp)}C
+                </div>
+                <div className="feelslike">
+                     Feels like {Math.round(weather.main.feels_like)}C {weather.weather[0].main },{weather.weather[0].description }
+                </div>
+                </div>
+                    <div className="wind">{weather.wind.speed}m/s NNW </div>
+                    <div className="humidity">Humidity:{weather.main.humidity}% </div>
+                    <div className="dewpoint">Dew pont:{weather.main.temp_min}C </div>
+                    <div className="pressure">{weather.main.pressure}hPa </div>
+                    <div className="visibility">Visibility: {weather.visibility}km </div>
+                    </div>
+                        ) : ('')}
+      </main>
+     </div>       
+    );
+             
+
+}
+export default Detailed
